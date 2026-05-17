d495190ea80f1a77dd9c371d4187b3355a88d817
Abhishekkumar503 <ak204479@gmail.com>
2026-05-17 18:48:00 +0530

    Configure Serenity

--- src/main/java/org/abis/playwright/toolshop/App.java
+++ src/main/java/org/abis/playwright/toolshop/App.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.abis.playwright.toolshop;
 
 /**
  * Hello world!
--- src/test/java/org/abis/playwright/toolshop/AppTest.java
+++ src/test/java/org/abis/playwright/toolshop/AppTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.abis.playwright.toolshop;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
