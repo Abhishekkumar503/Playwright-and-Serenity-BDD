9050d217cb182a4bcc8efa350c0aef75d87c7392
Abhishekkumar503 <ak204479@gmail.com>
2026-05-22 01:09:07 +0530

    Cucumber

---
+++ src/test/java/org/abis/playwright/toolshop/cucumber/CucumberTests.java
@@ -0,0 +1,21 @@
+package org.abis.playwright.toolshop.cucumber;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("/features")
+@ConfigurationParameter(
+        key="cucumber.plugin",
+        value = "net.serenitybdd.cucumber.core.plugin.SerenityReporterParallel," +
+//                "pretty," +
+                "html:target/cucumber-reports/cucumber.html"
+)
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.abis.playwright.toolshop")
+public class CucumberTests {
+}
